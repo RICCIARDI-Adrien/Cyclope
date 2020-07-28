ef18cf1b6768feedaba9dca8a6370cb1de32856a
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2020-07-28 23:24:40 +0200

    [Software] Added automatic mounting of data partition on boot.

--- Software/cyclope/board/cyclope/post-build.sh
+++ Software/cyclope/board/cyclope/post-build.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 set -u
-set -e
 
 # Replace /var/log symlink to /tmp by a real directory (so logs are kept across reboots)
 rm -rf "${TARGET_DIR}"/var/log
 ln -s /media/data/var/log "${TARGET_DIR}"/var/log
+
+# Append data partition to fstab if it not present yet
+DATA_PARTITION=$(grep "/dev/mmcblk0p4" "${TARGET_DIR}"/etc/fstab)
+if [ -z "$DATA_PARTITION" ]
+then
+	echo "/dev/mmcblk0p4	/media/data	ext4	defaults,noatime	0	0" >> "${TARGET_DIR}"/etc/fstab
+fi

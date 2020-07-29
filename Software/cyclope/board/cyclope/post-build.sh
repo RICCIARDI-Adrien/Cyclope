4208daa769eb35f7250a3862b598f75f3cd989bd
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2020-07-29 11:01:30 +0200

    [Software] Added automatic mounting of boot partition on system boot.

--- Software/cyclope/board/cyclope/post-build.sh
+++ Software/cyclope/board/cyclope/post-build.sh
@@ -2,12 +2,20 @@
 
 set -u
 
-# Create data partition mount point
+# Create partitions mount point
+mkdir -p "${TARGET_DIR}"/media/boot
 mkdir -p "${TARGET_DIR}"/media/data
 
 # Replace /var/log symlink to /tmp by a real directory (so logs are kept across reboots)
 rm -rf "${TARGET_DIR}"/var/log
 ln -s /media/data/var/log "${TARGET_DIR}"/var/log
+
+# Append boot partition to fstab if it not present yet
+DATA_PARTITION=$(grep "/dev/mmcblk0p1" "${TARGET_DIR}"/etc/fstab)
+if [ -z "$DATA_PARTITION" ]
+then
+	echo "/dev/mmcblk0p1	/media/boot	vfat	defaults	0	0" >> "${TARGET_DIR}"/etc/fstab
+fi
 
 # Append data partition to fstab if it not present yet
 DATA_PARTITION=$(grep "/dev/mmcblk0p4" "${TARGET_DIR}"/etc/fstab)

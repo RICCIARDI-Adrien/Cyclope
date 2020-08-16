1736010e83a977ff872b4ab226e1c85720048393
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2020-08-16 12:10:21 +0200

    [Software][Cyclope controller] Added 'build' and 'install' Buildroot package rules.

--- Software/Raspberry_Pi/cyclope/package/cyclope-controller/cyclope-controller.mk
+++ Software/Raspberry_Pi/cyclope/package/cyclope-controller/cyclope-controller.mk
@@ -10,4 +10,12 @@ CYCLOPE_CONTROLLER_VERSION = 1.0
 CYCLOPE_CONTROLLER_SITE_METHOD = local
 CYCLOPE_CONTROLLER_SITE = $(BR2_EXTERNAL_CYCLOPE_PATH)/package/cyclope-controller/src
 
+define CYCLOPE_CONTROLLER_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
+
+define CYCLOPE_CONTROLLER_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/cyclope-controller $(TARGET_DIR)/usr/sbin
+endef
+
 $(eval $(generic-package))

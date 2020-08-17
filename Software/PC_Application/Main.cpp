9a859a60f260f71a7dcb79de9e177c965bb86116
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2020-08-17 08:14:20 +0200

    [Software][PC application] Added configuration module initialization and uninitialization.

--- Software/PC_Application/Main.cpp
+++ Software/PC_Application/Main.cpp
@@ -2,6 +2,7 @@
  * Cyclope robot mission control software.
  * @author Adrien RICCIARDI
  */
+#include <Configuration.hpp>
 #include <MainWindow.hpp>
 #include <QApplication>
 
@@ -9,8 +10,18 @@ int main(int argc, char *argv[])
 {
     QApplication application(argc, argv);
 
+    // Load application configuration as soon as possible
+    if (Configuration::initialize() != 0) return -1;
+
+    // Create application main window
     MainWindow mainWindow;
     mainWindow.showMaximized();
 
-    return application.exec();
+    // Run application main loop
+    int result = application.exec();
+
+    // Release allocated resources
+    Configuration::uninitialize();
+
+    return result;
 }

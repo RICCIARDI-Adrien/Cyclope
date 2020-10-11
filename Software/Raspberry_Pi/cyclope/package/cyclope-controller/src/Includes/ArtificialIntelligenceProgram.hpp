ce882566209b09d6cdd91a164faae0a42eea5c43
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2020-10-11 22:43:29 +0200

    [Software][Cyclope controller] Started AI namespace implementation.

---
+++ Software/Raspberry_Pi/cyclope/package/cyclope-controller/src/Includes/ArtificialIntelligenceProgram.hpp
@@ -0,0 +1,14 @@
+/** @file ArtificialIntelligenceProgram.hpp
+ * Gather all available AI programs.
+ * @author Adrien RICCIARDI
+ */
+#ifndef HPP_ARTIFICIAL_INTELLIGENCE_HPP
+#define HPP_ARTIFICIAL_INTELLIGENCE_HPP
+
+namespace ArtificialIntelligenceProgram
+{
+	/** Follow a green object while it is visible by the robot and it is not too close. */
+	void followObject();
+}
+
+#endif
---
+++ Software/Raspberry_Pi/cyclope/package/cyclope-controller/src/Sources/ArtificialIntelligenceProgram.cpp
@@ -0,0 +1,13 @@
+/** @file ArtificialIntelligenceProgram.cpp
+ * See ArtificialIntelligenceProgram.hpp for description.
+ * @author Adrien RICCIARDI
+ */
+#include <ArtificialIntelligenceProgram.hpp>
+
+namespace ArtificialIntelligenceProgram
+{
+	void followObject()
+	{
+		// TODO
+	}
+}

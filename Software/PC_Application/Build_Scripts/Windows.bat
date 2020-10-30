46c16a31ac936e36bee3d915dd3a2295e4de394f
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2020-10-30 22:36:51 +0100

    [Software][PC application] Started Windows build script implementation. Application can currently be successfully built and dependencies are deployed.

---
+++ Software/PC_Application/Build_Scripts/Windows.bat
@@ -0,0 +1,53 @@
+@REM Find Qt "bin" directory
+@for /F "delims=" %%a in ('dir /B C:\Qt\5.15.*') do @set DETECTED_QT_VERSION=%%a%
+
+@REM Select toolchain according to build target
+@if "%1" == "32" (
+	@set QT_BINARIES_PATH=C:\Qt\%DETECTED_QT_VERSION%\mingw81_32\bin
+	@set BUILD_DIRECTORY=%TEMP%\Cyclope_Build_32_Bits
+	@set RELEASE_DIRECTORY=%TEMP%\Cyclope_Release_32_Bits
+	@set "PATH=C:\Qt\Tools\mingw810_32\bin;C:\Qt\%DETECTED_QT_VERSION%\mingw81_32\bin;%PATH%"
+) else if "%1" == "64" (
+	@set QT_BINARIES_PATH=C:\Qt\%DETECTED_QT_VERSION%\mingw81_64\bin
+	@set BUILD_DIRECTORY=%TEMP%\Cyclope_Build_64_Bits
+	@set RELEASE_DIRECTORY=%TEMP%\Cyclope_Release_64_Bits
+	@set "PATH=C:\Qt\Tools\mingw810_64\bin;C:\Qt\%DETECTED_QT_VERSION%\mingw81_64\bin;%PATH%"
+) else (
+	@echo Please specify build target architecture bits. Put "32" or "64" as first command line argument.
+	@goto End
+)
+@REM Make sure QT_BINARIES_PATH path is valid
+@echo Detected Qt version : %DETECTED_QT_VERSION%, detected Qt binaries path : %QT_BINARIES_PATH%
+@if not exist %QT_BINARIES_PATH%\qmake.exe (
+	@echo Invalid Qt "bin" directory. Make sure Qt is installed in "C:\Qt" directory.
+	@goto End
+)
+
+@set SOURCES_DIRECTORY=%CD%\..
+
+@REM Prepare directories
+rmdir /Q /S %BUILD_DIRECTORY%
+rmdir /Q /S %RELEASE_DIRECTORY%
+mkdir %BUILD_DIRECTORY%
+mkdir %RELEASE_DIRECTORY%
+mkdir %RELEASE_DIRECTORY%\Cyclope
+
+@REM Build application
+cd %BUILD_DIRECTORY%
+%QT_BINARIES_PATH%\qmake -spec win32-g++ %SOURCES_DIRECTORY%
+mingw32-make.exe -j%NUMBER_OF_PROCESSORS%
+
+@REM Copy relevant files to release path
+copy %BUILD_DIRECTORY%\release\Cyclope.exe %RELEASE_DIRECTORY%\Cyclope
+
+@REM Embed all needed libraries
+cd %RELEASE_DIRECTORY%\Cyclope
+@REM Do not use --release flag anymore with Qt 5.15.x (see QTBUG-84567)
+%QT_BINARIES_PATH%\windeployqt --compiler-runtime Cyclope.exe
+
+@REM TODO Compress application to a ZIP archive
+
+@REM Return to original directory
+@cd %SOURCES_DIRECTORY%\Build_Scripts
+
+:End

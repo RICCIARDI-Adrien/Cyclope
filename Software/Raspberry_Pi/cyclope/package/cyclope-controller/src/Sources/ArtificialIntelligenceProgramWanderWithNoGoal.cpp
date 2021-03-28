7ba7fa064dd9c5d9cd3de98d1416f6e57e4d7a44
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2021-03-28 23:51:55 +0200

    [Software][Cyclope controller] Added the begining of the 'wander with no goal' AI program. It is currently able to avoid not too close obstacles.

--- Software/Raspberry_Pi/cyclope/package/cyclope-controller/src/Sources/ArtificialIntelligenceProgramWanderWithNoGoal.cpp
+++ Software/Raspberry_Pi/cyclope/package/cyclope-controller/src/Sources/ArtificialIntelligenceProgramWanderWithNoGoal.cpp
@@ -5,6 +5,8 @@
 #include <ArtificialIntelligenceProgram.hpp>
 #include <cstdio>
 #include <Lidar.hpp>
+#include <Light.hpp>
+#include <Motor.hpp>
 #include <Network.hpp>
 #include <unistd.h>
 
@@ -12,19 +14,57 @@ namespace ArtificialIntelligenceProgram
 {
 	void wanderWithNoGoal()
 	{
-		int distanceFromAngles[LIDAR_ANGLES_COUNT], minimumDistance, maximumDistance;
+		const int MAXIMUM_DISTANCE_MILLIMETER = 800; // The maximum distance taken into account by the robot
+		const int OBSTACLE_AVOIDANCE_DISTANCE_MILLIMETER = 400; // The distance at which the robot will change its direction to avoid the object
+		int distanceFromAngles[LIDAR_ANGLES_COUNT], distance, leftDistance, rightDistance;
+		
+		// Reset robot state
+		Motor::setRobotMotion(Motor::ROBOT_MOTION_STOP);
+		Light::setEnabled(false);
+		memset(distanceFromAngles, 0, sizeof(distanceFromAngles)); // Prevent the robot from running until some valid data was processed
 		
 		// Start measuring distances
 		Lidar::setEnabled(true);
+		
+		// Wait for lidar to return meaningful information
+		do
+		{
+			Lidar::getLastDistances(distanceFromAngles);
+			Lidar::getDistanceRangeLimits(distanceFromAngles, 315, 45, &distance, nullptr);
+		} while (distance < 40); // Distance can't be less than 40mm, as this is the clearance from the lidar sensor to the chassis edge
 		
 		while (Network::isProgramRunning())
 		{
 			Lidar::getLastDistances(distanceFromAngles);
 			
-			// TEST
-			Lidar::getDistanceRangeLimits(distanceFromAngles, 350, 10, &minimumDistance, &maximumDistance);
-			printf("min=%d, max=%d\n", minimumDistance, maximumDistance);
-			if ((minimumDistance > 300) && (maximumDistance < 1000)) printf("OBSTACLE !!!\n");
+			// Search for the longest obstacle-free distance in front of the robot (using a field of view of 90°)
+			Lidar::getDistanceRangeLimits(distanceFromAngles, 315, 45, &distance, nullptr);
+			
+			// Clamp the distance to the maximum taken into account here
+			if (distance > MAXIMUM_DISTANCE_MILLIMETER) distance = MAXIMUM_DISTANCE_MILLIMETER;
+			
+			// Is an obstacle becoming close ?
+			if (distance < OBSTACLE_AVOIDANCE_DISTANCE_MILLIMETER)
+			{
+				Light::setEnabled(true);
+				
+				// Is there more room to go on the left or on the right (keep 10° each side as hysteresis) ?
+				Lidar::getDistanceRangeLimits(distanceFromAngles, 315, 350, &leftDistance, nullptr);
+				Lidar::getDistanceRangeLimits(distanceFromAngles, 10, 45, &rightDistance, nullptr);
+				
+				// Turn the the location with the less close obstacle
+				if (leftDistance > rightDistance) Motor::setRobotMotion(Motor::ROBOT_MOTION_FORWARD_LEFT);
+				else Motor::setRobotMotion(Motor::ROBOT_MOTION_FORWARD_RIGHT);
+			}
+			// No obstacle at sight, go straight
+			else
+			{
+				Light::setEnabled(false);
+				
+				Motor::setRobotMotion(Motor::ROBOT_MOTION_FORWARD);
+			}
+			
+			// React not too fast to avoid creating current bursts with motors
 			usleep(100000);
 		}
 		
